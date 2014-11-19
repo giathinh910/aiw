79c09687587127061b2838c1a54547255f0f1428
Thinh Bui <thinhbg@haintheme.com>
2014-11-19 22:02:32 +0700

    Add permit for all planet fields. Customize planet index

--- app/admin/planet.rb
+++ app/admin/planet.rb
@@ -14,5 +14,12 @@ ActiveAdmin.register Planet do
   #   permitted
   # end
 
+permit_params :name, :mass, :diameter, :density, :gravity, :escape_velocity, :rotation_period, :day_lenght, :distance_from_sun, :perihelion, :aphelion, :orbital_period, :orbital_velocity, :orbital_Inclination, :axis_inclination, :weight, :temperature, :surface_pressure, :atmospheric_composition, :satellite, :ring
 
+  index do
+    column "Tên", :name, :sortable => :price
+    column "Ngày tạo", :created_at
+    column "Ngày sửa gần nhất", :modified_at
+    actions
+  end
 end
