3573fa681991ffee2b49aba38617d63a77787d40
Thinh Bui <thinhbg@haintheme.com>
2014-11-26 21:52:35 +0700

    Add validation for planet, satellite and admin_user model

--- app/models/satellite.rb
+++ app/models/satellite.rb
@@ -1,3 +1,4 @@
 class Satellite < ActiveRecord::Base
   belongs_to :planet
+  validates_presence_of :name, :planet_id, :year_discovered, :discoverer, :distance_from_planet, :orbital_period, :avatar, :detail
 end
