3c4b6a4112a6e772e632071b5902e7781d7d7cb0
Thinh Bui <thinhbg@haintheme.com>
2014-11-28 07:58:41 +0700

    Separate header layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @planet_avatar = "background-image: url(http://localhost/www/aiw2014_astronomy/public/images/planet-1.png); box-shadow: 0px 0px 100px rgba(255, 152, 48, 0.5)"
   end
 
   def categories
--- app/controllers/planet_controller.rb
+++ app/controllers/planet_controller.rb
@@ -2,10 +2,12 @@ class PlanetController < ApplicationController
 
   def all
   	@planets = Planet.all
+  	@planet_avatar = "background-image: url(http://localhost/www/aiw2014_astronomy/public/images/planet-3.png); box-shadow: 0px 0px 100px rgba(255, 152, 48, 0.5)"
   end
 
   def detail
   	@planet = Planet.find(params[:id])
+  	@planet_avatar = "background-image: url(" + @planet.avatar + "); box-shadow: 0px 0px 100px rgba(255, 255, 255, 0.5)"
   end
 
 end
