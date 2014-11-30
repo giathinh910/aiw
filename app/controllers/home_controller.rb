46ae61ae9971438c5c5d6596c674ec06e6350d0a
Thinh Bui <thinhbg@haintheme.com>
2014-11-30 16:48:55 +0700

    Update links in homepage, main-nav

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @planet_avatar = "background-image: url(http://localhost/www/aiw2014_astronomy/public/images/planet-1.png); box-shadow: 0px 0px 100px rgba(255, 152, 48, 0.5)"
+    @planet_avatar = "background-image: url(http://localhost/www/aiw2014_astronomy/public/images/planet-5.png); box-shadow: 0px 0px 100px rgba(90, 137, 147, 1)"
   end
 
   def categories
